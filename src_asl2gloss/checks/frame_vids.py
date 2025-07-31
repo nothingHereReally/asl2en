d2de18a3e2450d635d4b25b3d67799b8c276b0f4
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2025-07-31 16:32:27 +0800

    changed to also show video files

--- src_asl2gloss/checks/frame_vids.py
+++ src_asl2gloss/checks/frame_vids.py
@@ -16,6 +16,7 @@ if __name__=="__main__":
         max_qframe: int= 0
         q_vids_successOpened: int= 0
         qframe_dist: dict= {}
+        qframe_dist_vidfile: dict= {}
         # for gloss in wlasl_ann:
         #     for svideo in gloss['instances']:
         #         vidfile= f"{WLASL_VID_DIR}{svideo['video_id']}.mp4"
@@ -28,23 +29,28 @@ if __name__=="__main__":
         #                 q_vids_successOpened +=1
         #                 try:
         #                     qframe_dist[curr_vidqframe] +=1
+        #                     qframe_dist_vidfile[curr_vidqframe].append(svideo['video_id'])
         #                 except KeyError:
         #                     qframe_dist[curr_vidqframe]= 1
+        #                     qframe_dist_vidfile[curr_vidqframe]= [svideo['video_id']]
         #             vidfile.release()
         # # min_qframe
         # # max_qframe
         # # q_vids_successOpened
         # # qframe_dist
         # with open(f"{PROJ_ROOT}src_asl2gloss/checks/data_frame_vids.py.pickle", "wb") as f:
-        #     pdump([min_qframe, max_qframe, q_vids_successOpened, qframe_dist], f)
+        #     pdump([min_qframe, max_qframe, q_vids_successOpened, qframe_dist, qframe_dist_vidfile], f)
         with open(f"{PROJ_ROOT}src_asl2gloss/checks/data_frame_vids.py.pickle", "rb") as f:
             tmp: list= pload(f)
             min_qframe= tmp[0]
             max_qframe= tmp[1]
             q_vids_successOpened= tmp[2]
             qframe_dist= tmp[3]
+            qframe_dist_vidfile= tmp[4]
         print(f"MINimum quantity of frames amongs ALL video( MP4 ) files --> {min_qframe}")
+        print(f"MINimum quantity of frames files are {qframe_dist_vidfile[min_qframe]}")
         print(f"MAXimum quantity of frames amongs ALL video( MP4 ) files --> {max_qframe}")
+        print(f"MAXimum quantity of frames files are {qframe_dist_vidfile[max_qframe]}")
         print(f"quantity video files successfully opened {q_vids_successOpened}")
         # for k in sorted(list(qframe_dist.keys())):
         #     print(f"qframe_dist {k} --> {qframe_dist[k]}")
@@ -67,9 +73,22 @@ if __name__=="__main__":
         print(f"MAXimum quantity videos has same frame of {sqf_maxQvids['k']} -- quantity of videos {sqf_maxQvids['val']}")
         print(f"---- box plot on frames with quantity of videos ----")
         print(f"quarter 1 ( minimum ) --> {qframe_dist_keyList[0]} frames( multiImg ) --> {qframe_dist[qframe_dist_keyList[0]]} qvids")
+        print(f"Q1 files: {qframe_dist_vidfile[qframe_dist_keyList[0]]}")
+
+
         bp_mid: int= int(len(qframe_dist_keyList)//2)
         print(f"quarter 2 ( median ) --> {qframe_dist_keyList[bp_mid//2]} frames( multiImg ) --> {qframe_dist[qframe_dist_keyList[bp_mid//2]]} qvids")
+        print(f"Q2 files: {qframe_dist_vidfile[qframe_dist_keyList[bp_mid//2]]}")
+
+
         print(f"quarter 3 ( median ) --> {qframe_dist_keyList[bp_mid]} frames( multiImg ) --> {qframe_dist[qframe_dist_keyList[bp_mid]]} qvids")
+        print(f"Q3 files: {qframe_dist_vidfile[qframe_dist_keyList[bp_mid]]}")
+
+
         print(f"quarter 4 ( median ) --> {qframe_dist_keyList[bp_mid+bp_mid//2]} frames( multiImg ) --> {qframe_dist[qframe_dist_keyList[bp_mid+bp_mid//2]]} qvids")
+        print(f"Q4 files: {qframe_dist_vidfile[qframe_dist_keyList[bp_mid+bp_mid//2]]}")
+
+
         print(f"quarter 5 ( maximum ) --> {qframe_dist_keyList[-1]} frames( multiImg ) --> {qframe_dist[qframe_dist_keyList[-1]]} qvids")
+        print(f"Q5 files: {qframe_dist_vidfile[qframe_dist_keyList[-1]]}")
 
