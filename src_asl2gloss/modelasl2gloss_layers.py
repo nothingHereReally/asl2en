7af4a61e61a8a8881c77bb4d92802b24617cf0ce
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2025-08-01 13:28:55 +0800

    layer for model 3

--- src_asl2gloss/modelasl2gloss_layers.py
+++ src_asl2gloss/modelasl2gloss_layers.py
@@ -86,3 +86,204 @@ data_out= Dense(
     name='batch_class'
 )(x)
 
+
+
+
+
+
+
+
+data_in= Input(
+    shape=(QUANTITY_FRAME, IMG_SIZE, IMG_SIZE, 3),
+    dtype=float32,
+    name='batch_vid'
+)
+
+
+
+x= Conv3D(
+    filters=8,
+    kernel_size=(3,3,3),
+    strides=(1,1,1),
+    padding='valid',
+    activation=relu,
+    dtype=float32,
+    name='conv_1'
+)(data_in)
+x= MaxPooling3D(
+    pool_size=(2,2,2),
+    strides=(1,1,1),
+    padding='valid',
+    dtype=float32,
+    name='maxpool_1'
+)(x)
+
+
+x= Conv3D(
+    filters=16,
+    kernel_size=(3,3,3),
+    strides=(1,1,1),
+    padding='valid',
+    activation=sigmoid,
+    dtype=float32,
+    name='conv_2'
+)(x)
+x= MaxPooling3D(
+    pool_size=(2,2,2),
+    strides=(1,1,1),
+    padding='valid',
+    dtype=float32,
+    name='maxpool_2'
+)(x)
+
+
+x= Conv3D(
+    filters=32,
+    kernel_size=(3,3,3),
+    strides=(1,1,1),
+    padding='valid',
+    activation=relu,
+    dtype=float32,
+    name='conv_3'
+)(x)
+x= MaxPooling3D(
+    pool_size=(5,5,5),
+    strides=(2,2,2),
+    padding='valid',
+    dtype=float32,
+    name='maxpool_3'
+)(x)
+
+
+x= Conv3D(
+    filters=64,
+    kernel_size=(3,3,3),
+    strides=(1,1,1),
+    padding='valid',
+    activation=sigmoid,
+    dtype=float32,
+    name='conv_4'
+)(x)
+x= MaxPooling3D(
+    pool_size=(3,5,5),
+    strides=(2,2,2),
+    padding='valid',
+    dtype=float32,
+    name='maxpool_4'
+)(x)
+
+
+x= Conv3D(
+    filters=128,
+    kernel_size=(3,3,3),
+    strides=(1,1,1),
+    padding='valid',
+    activation=relu,
+    dtype=float32,
+    name='conv_5'
+)(x)
+x= MaxPooling3D(
+    pool_size=(2,2,2),
+    strides=(1,1,1),
+    padding='valid',
+    dtype=float32,
+    name='maxpool_5'
+)(x)
+
+
+x= Conv3D(
+    filters=256,
+    kernel_size=(3,3,3),
+    strides=(1,1,1),
+    padding='valid',
+    activation=sigmoid,
+    dtype=float32,
+    name='conv_6'
+)(x)
+x= MaxPooling3D(
+    pool_size=(2,2,2),
+    strides=(1,1,1),
+    padding='valid',
+    dtype=float32,
+    name='maxpool_6'
+)(x)
+
+
+x= Conv3D(
+    filters=512,
+    kernel_size=(2,3,3),
+    strides=(1,1,1),
+    padding='valid',
+    activation=relu,
+    dtype=float32,
+    name='conv_7'
+)(x)
+x= MaxPooling3D(
+    pool_size=(1,5,5),
+    strides=(1,2,2),
+    padding='valid',
+    dtype=float32,
+    name='maxpool_7'
+)(x)
+
+
+x= Conv3D(
+    filters=1024,
+    kernel_size=(1,3,3),
+    strides=(1,1,1),
+    padding='valid',
+    activation=sigmoid,
+    dtype=float32,
+    name='conv_8'
+)(x)
+x= MaxPooling3D(
+    pool_size=(1,2,2),
+    strides=(1,1,1),
+    padding='valid',
+    dtype=float32,
+    name='maxpool_8'
+)(x)
+
+
+x= Conv3D(
+    filters=2048,
+    kernel_size=(1,3,3),
+    strides=(1,2,2),
+    padding='valid',
+    activation=relu,
+    dtype=float32,
+    name='conv_9'
+)(x)
+x= MaxPooling3D(
+    pool_size=(1,3,3),
+    strides=(1,1,1),
+    padding='valid',
+    dtype=float32,
+    name='maxpool_9'
+)(x)
+
+
+# # x= Conv3D(
+# #     filters=2048,
+# #     kernel_size=(1,3,3),
+# #     strides=(1,1,1),
+# #     padding='valid',
+# #     activation=relu,
+# #     dtype=float32,
+# #     name='conv_9'
+# # )(x)
+
+
+x= Flatten()(x)
+x= Dense(
+    units=TOTAL_GLOSS_UNIQ//100,
+    activation=sigmoid,
+    dtype=float32,
+    name='dense_1'
+)(x)
+data_out= Dense(
+    units=TOTAL_GLOSS_UNIQ,
+    activation=softmax,
+    dtype=float32,
+    name='batch_class'
+)(x)
