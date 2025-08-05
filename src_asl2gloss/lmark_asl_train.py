f59514e84c8af5539b350baba06141c2f27d6195
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2025-08-05 13:35:39 +0800

    model v12 finally done training
    track model v12 file and model v12 architecture layers
    very similar performance to model v10, about 1 day to
    finish 1 training

--- src_asl2gloss/lmark_asl_train.py
+++ src_asl2gloss/lmark_asl_train.py
@@ -26,24 +26,25 @@ from .lmark_essential_draw import getSkeletonFrames
 if __name__=="__main__":
     # model: Model= Model(
     #     inputs=data_in,
-    #     outputs=data_out
+    #     outputs=[data_out]
     # )
     # model.compile(
-    #     optimizer=Adam(learning_rate=0.001),
+    #     optimizer=Adam(learning_rate=0.0001),
+    #     # loss={"batch_class": sparse_categorical_crossentropy},
     #     loss=sparse_categorical_crossentropy,
     #     metrics=['accuracy']
     # )
-    model= load_model(f"{PROJ_ROOT}model/aslvid2gloss_v10.1.keras")
+    model= load_model(f"{PROJ_ROOT}model/aslvid2gloss_v12.3.keras")
     model.summary()
     # print(f"TOTAL_TRAIN_FILE//TRAIN_BATCH {TOTAL_TRAIN_FILE//TRAIN_BATCH}")
     model.fit(
-        getdata(isSimg=True),
+        getdata(),
         epochs=5,
         steps_per_epoch=int(ceil(TOTAL_TRAIN_FILE/TRAIN_BATCH))
     )
     print(f"proj_root {PROJ_ROOT}")
-    model.save(f"{PROJ_ROOT}model/aslvid2gloss_v10.2.keras")
-    # loadModel= load_model(f"{PROJ_ROOT}model/aslvid2gloss_v10.2.keras")
+    model.save(f"{PROJ_ROOT}model/aslvid2gloss_v12.4.keras")
+    # loadModel= load_model(f"{PROJ_ROOT}model/aslvid2gloss_v12.4.keras")
     # loadModel.summary()
     # shouldBeBook= loadModel.predict(getSkeletonFrames(f"{PROJ_ROOT}dataset/wlasl_dataset/videos/07092.mp4").reshape((1, QUANTITY_FRAME, IMG_SIZE, IMG_SIZE, 3)).astype(float32)/255.0)
     # print(f"{wlasl_READY['label_id2gloss'][argmax(shouldBeBook[0], axis=-1)]} --> accuracy {shouldBeBook[0][argmax(shouldBeBook[0], axis=-1)]*100}%")
