4b2d69590075ee9b9c8a2dacffd39510e23d5a6c
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2025-08-06 10:32:12 +0800

    finally done training model v9
    but still model v10 and v12 is better,
    compared to model v9

--- src_asl2gloss/lmark_asl_train.py
+++ src_asl2gloss/lmark_asl_train.py
@@ -9,6 +9,7 @@ from keras.src.losses import sparse_categorical_crossentropy
 from keras.src.models import Model
 from keras.src.optimizers import Adam
 from keras.src.saving import load_model
+from keras.src.callbacks import EarlyStopping, ReduceLROnPlateau
 from numpy import argmax, float32
 
 from .lmark_constant import TOTAL_TRAIN_FILE, TRAIN_BATCH, PROJ_ROOT
@@ -19,32 +20,46 @@ from .lmark_constant import IMG_SIZE, PROJ_ROOT, QUANTITY_FRAME, WLASL_VID_DIR, 
 from .modelasl2gloss_layers import data_in, data_out
 from .lmark_essential_draw import getSkeletonFrames, getdata
 from .lmark_essential_draw import getSkeletonFrames
+from .checks.test_asl_vid2gloss_model import test
 
 
     
 
 if __name__=="__main__":
+    test(f"{PROJ_ROOT}model/aslvid2gloss_v9.keras")
     # model: Model= Model(
     #     inputs=data_in,
     #     outputs=[data_out]
     # )
+    # d_lr= ReduceLROnPlateau(
+    #     monitor='accuracy',
+    #     factor=0.1,
+    #     mode='max',
+    #     patience=2,
+    #     min_lr=1.0e-7
+    # )
+    # sTraining= EarlyStopping(
+    #     monitor='loss',
+    #     min_delta=0.001,
+    #     patience=2,
+    #     mode='min'
+    # )
     # model.compile(
     #     optimizer=Adam(learning_rate=0.0001),
-    #     # loss={"batch_class": sparse_categorical_crossentropy},
     #     loss=sparse_categorical_crossentropy,
     #     metrics=['accuracy']
     # )
-    model= load_model(f"{PROJ_ROOT}model/aslvid2gloss_v12.3.keras")
-    model.summary()
-    # print(f"TOTAL_TRAIN_FILE//TRAIN_BATCH {TOTAL_TRAIN_FILE//TRAIN_BATCH}")
-    model.fit(
-        getdata(),
-        epochs=5,
-        steps_per_epoch=int(ceil(TOTAL_TRAIN_FILE/TRAIN_BATCH))
-    )
-    print(f"proj_root {PROJ_ROOT}")
-    model.save(f"{PROJ_ROOT}model/aslvid2gloss_v12.4.keras")
-    # loadModel= load_model(f"{PROJ_ROOT}model/aslvid2gloss_v12.4.keras")
-    # loadModel.summary()
-    # shouldBeBook= loadModel.predict(getSkeletonFrames(f"{PROJ_ROOT}dataset/wlasl_dataset/videos/07092.mp4").reshape((1, QUANTITY_FRAME, IMG_SIZE, IMG_SIZE, 3)).astype(float32)/255.0)
-    # print(f"{wlasl_READY['label_id2gloss'][argmax(shouldBeBook[0], axis=-1)]} --> accuracy {shouldBeBook[0][argmax(shouldBeBook[0], axis=-1)]*100}%")
+    # model.summary()
+    # # print(f"TOTAL_TRAIN_FILE//TRAIN_BATCH {TOTAL_TRAIN_FILE//TRAIN_BATCH}")
+    # model.fit(
+    #     getdata(),
+    #     epochs=12,
+    #     steps_per_epoch=int(ceil(TOTAL_TRAIN_FILE/TRAIN_BATCH)),
+    #     callbacks=[d_lr, sTraining]
+    # )
+    # print(f"proj_root {PROJ_ROOT}")
+    # model.save(f"{PROJ_ROOT}model/aslvid2gloss_v9.keras")
+    # # loadModel= load_model(f"{PROJ_ROOT}model/aslvid2gloss_v9.keras")
+    # # loadModel.summary()
+    # # shouldBeBook= loadModel.predict(getSkeletonFrames(f"{PROJ_ROOT}dataset/wlasl_dataset/videos/07092.mp4").reshape((1, QUANTITY_FRAME, IMG_SIZE, IMG_SIZE, 3)).astype(float32)/255.0)
+    # # print(f"{wlasl_READY['label_id2gloss'][argmax(shouldBeBook[0], axis=-1)]} --> accuracy {shouldBeBook[0][argmax(shouldBeBook[0], axis=-1)]*100}%")
--- src_asl2gloss/modelasl2gloss_layers.py
+++ src_asl2gloss/modelasl2gloss_layers.py
@@ -1,4 +1,5 @@
-from keras.src.layers import Conv2D, Dense, Dropout, Flatten, Input, MaxPooling2D, ReLU
+from keras.src.activations.activations import ReLU
+from keras.src.layers import Conv2D, Conv3D, Dense, Dropout, Flatten, Input, MaxPooling2D, MaxPooling3D, Reshape, TimeDistributed
 from keras.src.activations import softmax
 from numpy import float32
 
@@ -7,153 +8,180 @@ from .lmark_constant import QUANTITY_FRAME, IMG_SIZE, TOTAL_GLOSS_UNIQ
 
 
 data_in= Input(
-    shape=(QUANTITY_FRAME*IMG_SIZE, IMG_SIZE, 3),
+    shape=(QUANTITY_FRAME, IMG_SIZE, IMG_SIZE, 3),
     dtype=float32,
     name='batch_vid'
 )
 
 
-
-x= Conv2D(
+x= TimeDistributed(Conv2D(
     filters=8,
     kernel_size=(3,3),
     strides=(1,1),
     padding='valid',
     data_format='channels_last',
     activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+),
     name='cnn2d_1'
 )(data_in)
-x= MaxPooling2D(
-    pool_size=(2,2),
+x= MaxPooling3D(
+    pool_size=(1,2,2),
     padding='valid',
     data_format='channels_last',
     name='mp2d_1'
+)(x)
+x= Conv3D(
+    filters=8,
+    kernel_size=(3,1,1),
+    strides=(1,1,1),
+    padding='valid',
+    data_format='channels_last',
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    name='cnn3d_1'
+)(x)
+x= MaxPooling3D(
+    pool_size=(2,1,1),
+    padding='valid',
+    data_format='channels_last',
+    name='mp3d_1'
 )(x)
 x= Dropout(
     rate=0.2,
     name='do_1'
 )(x)
-x= Conv2D(
+x= TimeDistributed(Conv2D(
     filters=16,
     kernel_size=(3,3),
     strides=(1,1),
     padding='valid',
     data_format='channels_last',
     activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+),
     name='cnn2d_2'
 )(x)
-x= MaxPooling2D(
-    pool_size=(2,2),
+x= MaxPooling3D(
+    pool_size=(1,2,2),
     padding='valid',
     data_format='channels_last',
     name='mp2d_2'
 )(x)
+x= Conv3D(
+    filters=16,
+    kernel_size=(3,1,1),
+    strides=(1,1,1),
+    padding='valid',
+    data_format='channels_last',
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    name='cnn3d_2'
+)(x)
+# x= MaxPooling3D(
+#     pool_size=(2,1,1),
+#     padding='valid',
+#     data_format='channels_last',
+#     name='mp3d_2'
+# )
 x= Dropout(
     rate=0.2,
     name='do_2'
 )(x)
-x= Conv2D(
-    filters=24,
+x= TimeDistributed(Conv2D(
+    filters=32,
     kernel_size=(3,3),
     strides=(1,1),
     padding='valid',
     data_format='channels_last',
     activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+),
     name='cnn2d_3'
 )(x)
-x= MaxPooling2D(
-    pool_size=(2,2),
+x= MaxPooling3D(
+    pool_size=(1,2,2),
     padding='valid',
     data_format='channels_last',
     name='mp2d_3'
+)(x)
+x= Conv3D(
+    filters=32,
+    kernel_size=(3,1,1),
+    strides=(1,1,1),
+    padding='valid',
+    data_format='channels_last',
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    name='cnn3d_3'
 )(x)
 x= Dropout(
     rate=0.2,
     name='do_3'
 )(x)
-x= Conv2D(
-    filters=24,
+x= TimeDistributed(Conv2D(
+    filters=32,
     kernel_size=(3,3),
     strides=(1,1),
     padding='valid',
     data_format='channels_last',
     activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+),
     name='cnn2d_4'
 )(x)
-x= MaxPooling2D(
-    pool_size=(2,2),
+x= MaxPooling3D(
+    pool_size=(1,3,3),
     padding='valid',
     data_format='channels_last',
     name='mp2d_4'
+)(x)
+x= Conv3D(
+    filters=32,
+    kernel_size=(3,1,1),
+    strides=(1,1,1),
+    padding='valid',
+    data_format='channels_last',
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    name='cnn3d_4'
 )(x)
 x= Dropout(
     rate=0.2,
     name='do_4'
 )(x)
-x= Conv2D(
-    filters=24,
-    kernel_size=(3,3),
-    strides=(1,1),
-    padding='valid',
-    data_format='channels_last',
-    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
-    name='cnn2d_5'
-)(x)
-# x= MaxPooling2D(
-#     pool_size=(2,2),
-#     padding='valid',
-#     data_format='channels_last',
-#     name='mp2d_5'
-# )(x)
-# x= Dropout(
-#     rate=0.2,
-#     name='do_5'
-# )(x)
-# x= Conv2D(
-#     filters=24,
-#     kernel_size=(3,3),
-#     strides=(1,1),
+# x= Conv3D(
+#     filters=8,
+#     kernel_size=(3,1,1),
+#     strides=(1,1,1),
 #     padding='valid',
 #     data_format='channels_last',
 #     activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
-#     name='cnn2d_6'
+#     name='cnn3d_5'
 # )(x)
-# x= MaxPooling2D(
-#     pool_size=(2,2),
+# x= MaxPooling3D(
+#     pool_size=(2,1,1),
 #     padding='valid',
 #     data_format='channels_last',
-#     name='mp2d_6'
+#     name='mp3d_5'
 # )(x)
-# x= Dropout(
-#     rate=0.2,
-#     name='do_6'
-# )(x)
-# x= Conv2D(
-#     filters=24,
-#     kernel_size=(3,3),
-#     strides=(1,1),
+# x= Conv3D(
+#     filters=8,
+#     kernel_size=(3,1,1),
+#     strides=(1,1,1),
 #     padding='valid',
 #     data_format='channels_last',
 #     activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
-#     name='cnn2d_7'
+#     name='cnn3d_6'
 # )(x)
-# x= MaxPooling2D(
-#     pool_size=(2,2),
+# x= MaxPooling3D(
+#     pool_size=(2,1,1),
 #     padding='valid',
 #     data_format='channels_last',
-#     name='mp2d_7'
+#     name='mp3d_6'
 # )(x)
-# x= Dropout(
-#     rate=0.2,
-#     name='do_7'
+# x= Conv3D(
+#     filters=32,
+#     kernel_size=(3,1,1),
+#     strides=(1,1,1),
+#     padding='valid',
+#     data_format='channels_last',
+#     activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+#     name='cnn3d_7'
 # )(x)
+x= Flatten(name='flat')(x)
 
 
-x= Flatten(
-    name='flat_1'
-)(x)
-data_out= Dense(
-    units=TOTAL_GLOSS_UNIQ,
-    activation=softmax,
-    name='batch_class'
-)(x)
+data_out = Dense(TOTAL_GLOSS_UNIQ, activation=softmax, name='batch_class')(x)
