3e5af01e5654e186c8c155eb60baf4d8da4d5114
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2025-08-08 15:58:20 +0800

    started training for model v11 at 3:55pm

--- src_asl2gloss/model_layers.py
+++ src_asl2gloss/model_layers.py
@@ -1,5 +1,5 @@
 from keras.src.activations.activations import ReLU
-from keras.src.layers import Conv2D, Conv3D, Dense, Dropout, Flatten, Input, MaxPooling2D, MaxPooling3D, Reshape, TimeDistributed
+from keras.src.layers import Conv3D, Dense, Dropout, Flatten, Input, MaxPooling3D
 from keras.src.activations import softmax
 from numpy import float32
 
@@ -14,14 +14,13 @@ data_in= Input(
 )
 
 
-x= TimeDistributed(Conv2D(
+x= Conv3D(
     filters=8,
-    kernel_size=(3,3),
-    strides=(1,1),
+    kernel_size=(1,3,3),
+    strides=(1,1,1),
     padding='valid',
     data_format='channels_last',
     activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
-),
     name='cnn2d_1'
 )(data_in)
 x= MaxPooling3D(
@@ -46,17 +45,20 @@ x= MaxPooling3D(
     name='mp3d_1'
 )(x)
 x= Dropout(
-    rate=0.2,
+    rate=0.1,
     name='do_1'
 )(x)
-x= TimeDistributed(Conv2D(
+
+
+
+
+x= Conv3D(
     filters=16,
-    kernel_size=(3,3),
-    strides=(1,1),
+    kernel_size=(1,3,3),
+    strides=(1,1,1),
     padding='valid',
     data_format='channels_last',
     activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
-),
     name='cnn2d_2'
 )(x)
 x= MaxPooling3D(
@@ -81,17 +83,20 @@ x= Conv3D(
 #     name='mp3d_2'
 # )
 x= Dropout(
-    rate=0.2,
+    rate=0.1,
     name='do_2'
 )(x)
-x= TimeDistributed(Conv2D(
+
+
+
+
+x= Conv3D(
     filters=32,
-    kernel_size=(3,3),
-    strides=(1,1),
+    kernel_size=(1,3,3),
+    strides=(1,1,1),
     padding='valid',
     data_format='channels_last',
     activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
-),
     name='cnn2d_3'
 )(x)
 x= MaxPooling3D(
@@ -110,17 +115,20 @@ x= Conv3D(
     name='cnn3d_3'
 )(x)
 x= Dropout(
-    rate=0.2,
+    rate=0.1,
     name='do_3'
 )(x)
-x= TimeDistributed(Conv2D(
+
+
+
+
+x= Conv3D(
     filters=32,
-    kernel_size=(3,3),
-    strides=(1,1),
+    kernel_size=(1,3,3),
+    strides=(1,1,1),
     padding='valid',
     data_format='channels_last',
     activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
-),
     name='cnn2d_4'
 )(x)
 x= MaxPooling3D(
@@ -138,49 +146,6 @@ x= Conv3D(
     activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
     name='cnn3d_4'
 )(x)
-x= Dropout(
-    rate=0.2,
-    name='do_4'
-)(x)
-# x= Conv3D(
-#     filters=8,
-#     kernel_size=(3,1,1),
-#     strides=(1,1,1),
-#     padding='valid',
-#     data_format='channels_last',
-#     activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
-#     name='cnn3d_5'
-# )(x)
-# x= MaxPooling3D(
-#     pool_size=(2,1,1),
-#     padding='valid',
-#     data_format='channels_last',
-#     name='mp3d_5'
-# )(x)
-# x= Conv3D(
-#     filters=8,
-#     kernel_size=(3,1,1),
-#     strides=(1,1,1),
-#     padding='valid',
-#     data_format='channels_last',
-#     activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
-#     name='cnn3d_6'
-# )(x)
-# x= MaxPooling3D(
-#     pool_size=(2,1,1),
-#     padding='valid',
-#     data_format='channels_last',
-#     name='mp3d_6'
-# )(x)
-# x= Conv3D(
-#     filters=32,
-#     kernel_size=(3,1,1),
-#     strides=(1,1,1),
-#     padding='valid',
-#     data_format='channels_last',
-#     activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
-#     name='cnn3d_7'
-# )(x)
 x= Flatten(name='flat')(x)
 
 
