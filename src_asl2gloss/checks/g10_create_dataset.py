bb575497cc001cc1bbc729cf9cef36c7a44df026
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2025-08-10 20:57:52 +0800

    success loop, no error

--- src_asl2gloss/checks/g10_create_dataset.py
+++ src_asl2gloss/checks/g10_create_dataset.py
@@ -2,5 +2,10 @@ from ..lmark_constant import wlasl_READY_10
 
 
 if __name__=='__main__':
-    print('hello world')
+    for train in wlasl_READY_10['train']:
+        print(f"{train['video_id']} ---- {train['gloss_id']}")
+    for val in wlasl_READY_10['val']:
+        print(f"{val['video_id']} ---- {val['gloss_id']}")
+    for test in wlasl_READY_10['test']:
+        print(f"{test['video_id']} ---- {test['gloss_id']}")
 
