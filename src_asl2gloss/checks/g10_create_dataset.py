658a06adc6355fbfc0fa7db82b6ed207bfa38d65
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2025-08-11 01:15:18 +0800

    removed comments and restore back MIN_FRAMES_HAS_HANDS definition

--- src_asl2gloss/checks/g10_create_dataset.py
+++ src_asl2gloss/checks/g10_create_dataset.py
@@ -10,12 +10,6 @@ from ..lmark_constant import PROJ_ROOT, WLASL_VID_DIR, wlasl_READY_10
 
 
 if __name__=='__main__':
-    # for train in wlasl_READY_10['train']:
-    #     print(f"{train['video_id']} ---- {train['gloss_id']}")
-    # for val in wlasl_READY_10['val']:
-    #     print(f"{val['video_id']} ---- {val['gloss_id']}")
-    # for test in wlasl_READY_10['test']:
-    #     print(f"{test['video_id']} ---- {test['gloss_id']}")
 
 
     init_dir: str= f"{PROJ_ROOT}dataset/wlasl_dataset/"
