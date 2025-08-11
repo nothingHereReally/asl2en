bd860fe903dc11163910cfc4129fad76c6c9e332
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2025-08-11 14:24:56 +0800

    added directory constant to skeleton images

--- src_asl2gloss/lmark_constant.py
+++ src_asl2gloss/lmark_constant.py
@@ -42,6 +42,7 @@ del tmp_ready
 T10_GLOSS: int= int(len(wlasl_READY_10['label_id2gloss']))
 T10_TRAIN: int= int(len(wlasl_READY_10['train']))
 T10_VAL: int= int(len(wlasl_READY_10['val']))
+T10_DIR_IMG: str= f"{PROJ_ROOT}dataset/wlasl_dataset/ins_images/"
 TOTAL_GLOSS_UNIQ: int= int(len(wlasl_READY['label_id2gloss']))
 TOTAL_TRAIN_FILE: int= int(len(wlasl_READY['train']))
 TOTAL_VAL_FILE: int= int(len(wlasl_READY['val']))
