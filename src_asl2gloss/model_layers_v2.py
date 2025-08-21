57be863eb8f42676b5e544b353376f9ec0514a8b
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2025-08-22 00:24:32 +0800

    some improvement about 53% accuracy

--- src_asl2gloss/model_layers_v2.py
+++ src_asl2gloss/model_layers_v2.py
@@ -1,7 +1,7 @@
 from keras.src.activations.activations import ReLU
 from keras.src.layers import LSTM, Conv1D, Dropout, MaxPooling2D, Reshape, Dense, Input, TimeDistributed
 from keras.src.activations import softmax
-from numpy import float32
+from numpy import float32, float64
 
 
 from .lmark_constant_v2 import LM_Q_FACE, LM_Q_HAND, LM_Q_POSE, QUANTITY_FRAME, T10_GLOSS
@@ -12,65 +12,45 @@ data_in= Input(
     dtype=float32,
     name='batch_vid',
 )
-# not good only about 40% accurate
-x= TimeDistributed(Conv1D(
-    filters=8,
-    kernel_size=3,
-    strides=1,
-    padding='valid',
-    data_format='channels_last',
-    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0)
-),
-    name='cnn'
+x= Reshape(
+    target_shape=(QUANTITY_FRAME, -1),
+    dtype=float64,
+    name='reshape'
 )(data_in)
-x= MaxPooling2D(
-    pool_size=(1,2),
-    strides=(1,2),
-    padding='valid',
-    data_format='channels_last',
-    name='mp'
-)(x)
-x= TimeDistributed(Conv1D(
-    filters=16,
-    kernel_size=3,
-    strides=1,
-    padding='valid',
-    data_format='channels_last',
-    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0)
-),
-    name='cnn1'
-)(x)
-x= MaxPooling2D(
-    pool_size=(1,2),
-    strides=(1,2),
-    padding='valid',
-    data_format='channels_last',
-    name='mp1'
-)(x)
-x= TimeDistributed(LSTM(
-    units=128,
-    return_sequences=True
-),
+x= LSTM(
+    units=256,
+    return_sequences=True,
+    dtype=float64,
     name='lstm'
 )(x)
-x= TimeDistributed(LSTM(
-    units=128
-),
+x= LSTM(
+    units=256,
+    return_sequences=True,
+    dtype=float64,
     name='lstm1'
 )(x)
 x= LSTM(
-    units=64,
-    name='lstm2'
+    units=128,
+    dtype=float64,
+    name='lstm_out'
 )(x)
 x= Dropout(
-    rate=0.1
+    rate=0.1,
+    dtype=float64,
 )(x)
 
 
 x= Dense(
-    units=32,
+    units=128,
     activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
     name='ann'
+)(x)
+x= Dense(
+    units=64,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+    name='ann1'
 )(x)
 
 
