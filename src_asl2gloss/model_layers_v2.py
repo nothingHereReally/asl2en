34a1b81692c7715e8a71dcf0f9802073355fd087
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2025-08-24 00:27:13 +0800

    very close to reallity, finally done model v18.a
    due to accuracy on train, val and test are very close
    train accuracy approx. 54%
    val accuracy approx. 53%
    test accuracy approx. 53%

--- src_asl2gloss/model_layers_v2.py
+++ src_asl2gloss/model_layers_v2.py
@@ -1,5 +1,5 @@
 from keras.src.activations.activations import ReLU
-from keras.src.layers import LSTM, Add, Attention, Concatenate, Flatten, Normalization, Reshape, Dense, Input, TimeDistributed
+from keras.src.layers import LSTM, Add, Attention, BatchNormalization, Concatenate, Reshape, Dense, Input, TimeDistributed
 from keras.src.activations import softmax
 from numpy import float32, float64
 
@@ -565,71 +565,287 @@ att_h5= Add(name='add_h5')([q_ann_p5, v_ann_p5, att_h5])
 att_h6= Add(name='add_h6')([q_ann_p6, v_ann_p6, att_h6])
 att_h7= Add(name='add_h7')([q_ann_p7, v_ann_p7, att_h7])
 att_h8= Add(name='add_h8')([q_ann_p8, v_ann_p8, att_h8])
-att_h1= Normalization(name='norm_h1')(att_h1)
-att_h2= Normalization(name='norm_h2')(att_h2)
-att_h3= Normalization(name='norm_h3')(att_h3)
-att_h4= Normalization(name='norm_h4')(att_h4)
-att_h5= Normalization(name='norm_h5')(att_h5)
-att_h6= Normalization(name='norm_h6')(att_h6)
-att_h7= Normalization(name='norm_h7')(att_h7)
-att_h8= Normalization(name='norm_h8')(att_h8)
+att_h1= BatchNormalization(name='norm_h1')(att_h1)
+att_h2= BatchNormalization(name='norm_h2')(att_h2)
+att_h3= BatchNormalization(name='norm_h3')(att_h3)
+att_h4= BatchNormalization(name='norm_h4')(att_h4)
+att_h5= BatchNormalization(name='norm_h5')(att_h5)
+att_h6= BatchNormalization(name='norm_h6')(att_h6)
+att_h7= BatchNormalization(name='norm_h7')(att_h7)
+att_h8= BatchNormalization(name='norm_h8')(att_h8)
 conct= Concatenate(
     axis=-1,
     name='concat_8heads'
 )([att_h1, att_h2, att_h3, att_h4, att_h5, att_h6, att_h7, att_h8]) # shape is (22, 8*8) is (22, 64)
+# conct= SpatialDropout1D(
+#     rate=0.45,
+#     name='do_8heads'
+# )(conct)
+
+
+q2_ann_p1= TimeDistributed(Dense(
+    units=64,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+),
+    name='q2_ann_h1_2')(conct)
+q2_ann_p1= TimeDistributed(Dense(
+    units=32,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+),
+    name='q2_ann_h1_3')(q2_ann_p1)
+# q2_ann_p1= TimeDistributed(Dense(
+#     units=16,
+#     activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+#     dtype=float64,
+# ),
+#     name='q2_ann_h1_4')(q2_ann_p1)
+q2_ann_p1= TimeDistributed(Dense(
+    units=8,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+),
+    name='q2_ann_h1_5')(q2_ann_p1)
+
+v2_ann_p1= TimeDistributed(Dense(
+    units=64,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+),
+    name='v2_ann_h1_2')(conct)
+v2_ann_p1= TimeDistributed(Dense(
+    units=32,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+),
+    name='v2_ann_h1_3')(v2_ann_p1)
+# v2_ann_p1= TimeDistributed(Dense(
+#     units=16,
+#     activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+#     dtype=float64,
+# ),
+#     name='v2_ann_h1_4')(v2_ann_p1)
+v2_ann_p1= TimeDistributed(Dense(
+    units=8,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+),
+    name='v2_ann_h1_5')(v2_ann_p1)
+
+
+q2_ann_p2= TimeDistributed(Dense(
+    units=64,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+),
+    name='q2_ann_h2_2')(conct)
+q2_ann_p2= TimeDistributed(Dense(
+    units=32,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+),
+    name='q2_ann_h2_3')(q2_ann_p2)
+# q2_ann_p2= TimeDistributed(Dense(
+#     units=16,
+#     activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+#     dtype=float64,
+# ),
+#     name='q2_ann_h2_4')(q2_ann_p2)
+q2_ann_p2= TimeDistributed(Dense(
+    units=8,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+),
+    name='q2_ann_h2_5')(q2_ann_p2)
+
+v2_ann_p2= TimeDistributed(Dense(
+    units=64,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+),
+    name='v2_ann_h2_2')(conct)
+v2_ann_p2= TimeDistributed(Dense(
+    units=32,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+),
+    name='v2_ann_h2_3')(v2_ann_p2)
+# v2_ann_p2= TimeDistributed(Dense(
+#     units=16,
+#     activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+#     dtype=float64,
+# ),
+#     name='v2_ann_h2_4')(v2_ann_p2)
+v2_ann_p2= TimeDistributed(Dense(
+    units=8,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+),
+    name='v2_ann_h2_5')(v2_ann_p2)
+
+
+q2_ann_p3= TimeDistributed(Dense(
+    units=64,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+),
+    name='q2_ann_h3_2')(conct)
+q2_ann_p3= TimeDistributed(Dense(
+    units=32,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+),
+    name='q2_ann_h3_3')(q2_ann_p3)
+# q2_ann_p3= TimeDistributed(Dense(
+#     units=16,
+#     activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+#     dtype=float64,
+# ),
+#     name='q2_ann_h3_4')(q2_ann_p3)
+q2_ann_p3= TimeDistributed(Dense(
+    units=8,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+),
+    name='q2_ann_h3_5')(q2_ann_p3)
+
+v2_ann_p3= TimeDistributed(Dense(
+    units=64,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+),
+    name='v2_ann_h3_2')(conct)
+v2_ann_p3= TimeDistributed(Dense(
+    units=32,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+),
+    name='v2_ann_h3_3')(v2_ann_p3)
+# v2_ann_p3= TimeDistributed(Dense(
+#     units=16,
+#     activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+#     dtype=float64,
+# ),
+#     name='v2_ann_h3_4')(v2_ann_p3)
+v2_ann_p3= TimeDistributed(Dense(
+    units=8,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+),
+    name='v2_ann_h3_5')(v2_ann_p3)
+
+
+q2_ann_p4= TimeDistributed(Dense(
+    units=64,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+),
+    name='q2_ann_h4_2')(conct)
+q2_ann_p4= TimeDistributed(Dense(
+    units=32,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+),
+    name='q2_ann_h4_3')(q2_ann_p4)
+# q2_ann_p4= TimeDistributed(Dense(
+#     units=16,
+#     activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+#     dtype=float64,
+# ),
+#     name='q2_ann_h4_4')(q2_ann_p4)
+q2_ann_p4= TimeDistributed(Dense(
+    units=8,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+),
+    name='q2_ann_h4_5')(q2_ann_p4)
+
+v2_ann_p4= TimeDistributed(Dense(
+    units=64,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+),
+    name='v2_ann_h4_2')(conct)
+v2_ann_p4= TimeDistributed(Dense(
+    units=32,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+),
+    name='v2_ann_h4_3')(v2_ann_p4)
+# v2_ann_p4= TimeDistributed(Dense(
+#     units=16,
+#     activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+#     dtype=float64,
+# ),
+#     name='v2_ann_h4_4')(v2_ann_p4)
+v2_ann_p4= TimeDistributed(Dense(
+    units=8,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+),
+    name='v2_ann_h4_5')(v2_ann_p4)
+
+
+att2_h1= Attention(
+    name='att2_h1',
+    use_scale=True
+)([q2_ann_p1, v2_ann_p1, v2_ann_p1])
+att2_h2= Attention(
+    name='att2_h2',
+    use_scale=True
+)([q2_ann_p2, v2_ann_p2, v2_ann_p2])
+att2_h3= Attention(
+    name='att2_h3',
+    use_scale=True
+)([q2_ann_p3, v2_ann_p3, v2_ann_p3])
+att2_h4= Attention(
+    name='att2_h4',
+    use_scale=True
+)([q2_ann_p4, v2_ann_p4, v2_ann_p4])
+
+
+att2_h1= Add(name='add2_h1')([q2_ann_p1, v2_ann_p1, att2_h1])
+att2_h2= Add(name='add2_h2')([q2_ann_p2, v2_ann_p2, att2_h2])
+att2_h3= Add(name='add2_h3')([q2_ann_p3, v2_ann_p3, att2_h3])
+att2_h4= Add(name='add2_h4')([q2_ann_p4, v2_ann_p4, att2_h4])
+att2_h1= BatchNormalization(name='norm2_h1')(att2_h1)
+att2_h2= BatchNormalization(name='norm2_h2')(att2_h2)
+att2_h3= BatchNormalization(name='norm2_h3')(att2_h3)
+att2_h4= BatchNormalization(name='norm2_h4')(att2_h4)
+conct= Concatenate(
+    axis=-1,
+    name='concat_4heads'
+)([att2_h1, att2_h2, att2_h3, att2_h4]) # shape is (22, 4*8) is (22, 32)
+# conct= SpatialDropout1D(
+#     rate=0.2,
+#     name='do_4heads'
+# )(conct)
+ann= LSTM(
+    units=32
+)(conct)
 
 
 # ann= Flatten(
-#     name='flat'
+#     name='flat_4heads'
 # )(conct)
-# ann= Dense(
-#     units=1024,
-#     activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
-#     name='ann1024'
-# )(ann)
 # ann= Dense(
 #     units=512,
 #     activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
-#     name='ann512'
-# )(ann)
-# ann= Dense(
-#     units=256,
-#     activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
-#     name='ann256'
+#     dtype=float64,
+#     name='ann2_512'
 # )(ann)
 # ann= Dense(
 #     units=128,
 #     activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
-#     name='ann128'
+#     dtype=float64,
+#     name='ann2_128'
 # )(ann)
-# ann= Dense(
-#     units=64,
-#     activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
-#     name='ann64'
-# )(ann)
-lstm= LSTM(
-    units=64,
-    return_sequences=True,
-    name='lstm_long1'
-)(conct)
-lstm= LSTM(
-    units=64,
-    return_sequences=True,
-    name='lstm_long2'
-)(lstm)
-lstm= Add(
-    name='add_lstm_concat'
-)([lstm, conct])
-lstm= Normalization(name='norm_2ndpart')(lstm) # shape (22, 64)
-_, _, lstm= LSTM(
-    units=16,
-    return_state=True,
-    name='lstm_be_short'
-)(lstm)
-ann= Flatten()(lstm)
 ann= Dense(
-    units=16,
-    name='ann'
+    units=32,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+    name='ann2_32'
 )(ann)
 
 
