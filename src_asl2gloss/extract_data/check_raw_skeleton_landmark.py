66fd47066a1ed7bec8b60920f6da766d3f85a4bc
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2025-09-18 12:36:24 +0800

    added checking for gloss_id, all is good on dataset done checking

--- src_asl2gloss/extract_data/check_raw_skeleton_landmark.py
+++ src_asl2gloss/extract_data/check_raw_skeleton_landmark.py
@@ -5,6 +5,20 @@ from os import listdir
 PROJ_ROOT: str= f"{"/".join(__file__.rsplit("/")[:-3])}/"
 
 
+def isIncreaseOrEqual(prev: dict, current: dict) -> bool:
+    return bool(
+        prev['raw']<=current['raw'] and
+        prev['skeleton']<=current['skeleton'] and
+        prev['landmark']<=current['landmark'] and
+        prev['raw']==prev['skeleton'] and
+        prev['raw']==prev['landmark'] and
+        prev['skeleton']==prev['landmark'] and
+        current['raw']==current['skeleton'] and
+        current['raw']==current['landmark'] and
+        current['skeleton']==current['landmark']
+    )
+
+
 if __name__=='__main__':
     raw_image_json: str= f"{PROJ_ROOT}dataset/wlasl/wlasl.annotation.train_val_test.json"
     skeleton_json: str= f"{PROJ_ROOT}dataset/wlasl/wlasl.annotation.skeleton_image.train_val_test.json"
@@ -49,6 +63,42 @@ if __name__=='__main__':
                 print(f"json file at video_id {raw_image[tvt_idv][i]['video_id']} skeleton and landmark are not of equal quantity of images/landmarks")
                 print(f"skeleton image quantity files on json: {len(skeleton_[tvt_idv][i]['image'])}")
                 print(f"landmark quantity files on json: {len(landmark_[tvt_idv][i]['landmark'])}")
+
+            if i!=0:
+                prevG_raw_image: int= int(raw_image[tvt_idv][i-1]['gloss_id'])
+                prevG_skeleton: int= int(skeleton_[tvt_idv][i-1]['gloss_id'])
+                prevG_landmark: int= int(landmark_[tvt_idv][i-1]['gloss_id'])
+                currentG_raw_image: int= int(raw_image[tvt_idv][i]['gloss_id'])
+                currentG_skeleton: int= int(skeleton_[tvt_idv][i]['gloss_id'])
+                currentG_landmark: int= int(landmark_[tvt_idv][i]['gloss_id'])
+                if not isIncreaseOrEqual(
+                    prev={
+                        'raw': prevG_raw_image,
+                        'skeleton': prevG_skeleton,
+                        'landmark': prevG_landmark
+                    },
+                    current={
+                        'raw': currentG_raw_image,
+                        'skeleton': currentG_skeleton,
+                        'landmark': currentG_landmark
+                    }
+                ):
+                    all_good= False
+                    print(f"at video_id {raw_image[tvt_idv][i]['video_id']} gloss_id not increasing current( {currentG_raw_image} ) previous( {prevG_raw_image} )")
+                    print("or something else, full details:")
+                    print(f"prev gloss_id raw_image: {prevG_raw_image}")
+                    print(f"prev gloss_id skeleton: {prevG_skeleton}")
+                    print(f"prev gloss_id landmark: {prevG_landmark}")
+                    print(f"current gloss_id raw_image: {currentG_raw_image}")
+                    print(f"current gloss_id skeleton: {currentG_skeleton}")
+                    print(f"current gloss_id landmark: {currentG_landmark}\n")
+
+            if not (raw_image[tvt_idv][i]['video_id']!=skeleton_[tvt_idv][i]['video_id'] or raw_image!=landmark_[tvt_idv][i]['video_id']):
+                all_good= False
+                print(f"somethings wrong video_id doesn't match on 3( raw_image, skeleton, landmark ), details:")
+                print(f"video_id raw_image: {raw_image[tvt_idv][i]['video_id']}")
+                print(f"video_id skeleton: {skeleton_[tvt_idv][i]['video_id']}")
+                print(f"video_id landmark: {landmark_[tvt_idv][i]['video_id']}")
     if all_good:
         print("all files per video_id are of equal quantity, all is good")
     else:
