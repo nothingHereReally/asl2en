e629a72d89a6fa20cbb1399e4b6b6c690622c2fe
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2025-09-19 10:13:27 +0800

    changed to use attention mechanism

--- src_asl2gloss/model_layers.py
+++ src_asl2gloss/model_layers.py
@@ -1,154 +1,205 @@
-# from types import LambdaType
-# from typing import Any
 from keras.src.activations.activations import ReLU
-from keras.src.layers import Attention, Conv3D, Dense, Dropout, Flatten, Input, MaxPooling3D, Permute, Reshape
+from keras.src.layers import Add, Attention, Flatten, Normalization, Reshape, Dense, Input, TimeDistributed
 from keras.src.activations import softmax
-# from keras.ops import expand_dims
-# from tensorflow import reshape, convert_to_tensor
-# from keras.saving import register_keras_serializable
-from numpy import float32
+from numpy import float32, float64
 
 
-from .lmark_constant import QUANTITY_FRAME, IMG_SIZE, T10_GLOSS
+from .lmark_constant import LANDMARK_SHAPE, QUANTITY_FRAME, TRAIN_GLOSS
 
 
 data_in= Input(
-    shape=(QUANTITY_FRAME, IMG_SIZE, IMG_SIZE, 3),
+    shape=(QUANTITY_FRAME, LANDMARK_SHAPE[0], LANDMARK_SHAPE[1]),
     dtype=float32,
     name='batch_vid',
 )
-cx= Conv3D(
-    filters=8,
-    kernel_size=(1,3,3),
-    strides=(1,1,1),
-    padding='valid',
-    data_format='channels_last',
-    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
-    name='c0p1_cnn_2d'
+x= Reshape(
+    target_shape=(QUANTITY_FRAME, -1),
+    dtype=float64,
+    name='reshape'
 )(data_in)
-cx= MaxPooling3D(
-    pool_size=(1,2,2),
-    padding='valid',
-    data_format='channels_last',
-    name='c0p1_mp_2d'
-)(cx)
-cx= Conv3D(
-    filters=8,
-    kernel_size=(3,1,1),
-    strides=(1,1,1),
-    padding='valid',
-    data_format='channels_last',
-    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
-    name='c0p1_cnn_3d'
-)(cx)
-cx= MaxPooling3D(
-    pool_size=(2,1,1),
-    padding='valid',
-    data_format='channels_last',
-    name='c0p1_mp_3d'
-)(cx)
-cx= Reshape(
-    target_shape=(10, -1),
-    name='c0p1_reshape_b_att'
-)(cx)
-cx= Attention(
-    name='c0p1_att'
-)([cx,cx,cx])
-cx= Reshape(
-    target_shape=(10, 78, 78, 8),
-    name='c0p1_reshape_a_att'
-)(cx)
-cx= Dropout(
-    rate=0.1,
-    name='c0p1_do'
-)(cx)
-cx= Conv3D(
-    filters=16,
-    kernel_size=(1,3,3),
-    strides=(1,1,1),
-    padding='valid',
-    data_format='channels_last',
-    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
-    name='c0p2_cnn_2d'
-)(cx)
-cx= MaxPooling3D(
-    pool_size=(1,2,2),
-    padding='valid',
-    data_format='channels_last',
-    name='c0p2_mp_2d'
-)(cx)
-cx= Conv3D(
-    filters=16,
-    kernel_size=(3,1,1),
-    strides=(1,1,1),
-    padding='valid',
-    data_format='channels_last',
-    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
-    name='c0p2_cnn_3d'
-)(cx)
-cx= MaxPooling3D(
-    pool_size=(2,1,1),
-    padding='valid',
-    data_format='channels_last',
-    name='c0p2_mp_3d'
-)(cx)
-cx= Reshape(
-    target_shape=(4, -1),
-    name='c0p2_reshape_b_att'
-)(cx)
-cx= Attention(
-    name='c0p2_att'
-)([cx,cx,cx])
-cx= Reshape(
-    target_shape=(4, 38, 38, 16),
-    name='c0p2_reshape_a_att'
-)(cx)
-cx= Dropout(
-    rate=0.1,
-    name='c0p2_do'
-)(cx)
-cx= Conv3D(
-    filters=32,
-    kernel_size=(1,3,3),
-    strides=(1,1,1),
-    padding='valid',
-    data_format='channels_last',
-    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
-    name='c0p3_cnn_2d'
-)(cx)
-cx= MaxPooling3D(
-    pool_size=(1,2,2),
-    padding='valid',
-    data_format='channels_last',
-    name='c0p3_mp_2d'
-)(cx)
-cx= Conv3D(
-    filters=32,
-    kernel_size=(3,1,1),
-    strides=(1,1,1),
-    padding='valid',
-    data_format='channels_last',
-    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
-    name='c0p3_cnn_3d'
-)(cx)
-cx= MaxPooling3D(
-    pool_size=(2,1,1),
-    padding='valid',
-    data_format='channels_last',
-    name='c0p3_mp_3d'
-)(cx)
-cx= Reshape(
-    target_shape=(-1, 32),
-    name='c0p3_reshape_b_att'
-)(cx)
-cx= Permute(
-    dims=(2, 1),
-    name='transpose_b'
-)(cx)
-cx= Attention(
-    name='c0p3_att'
-)([cx,cx,cx])
-x= Flatten(name='flat')(cx)
 
 
-data_out = Dense(T10_GLOSS, activation=softmax, name='batch_class')(x)
+
+
+
+
+att_q_h1= TimeDistributed(Dense(
+    units=512,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+))(x)
+att_v_h1= TimeDistributed(Dense(
+    units=512,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+))(x)
+att_k_h1= TimeDistributed(Dense(
+    units=512,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+))(x)
+
+att_q_h2= TimeDistributed(Dense(
+    units=512,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+))(x)
+att_v_h2= TimeDistributed(Dense(
+    units=512,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+))(x)
+att_k_h2= TimeDistributed(Dense(
+    units=512,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+))(x)
+
+att_q_h3= TimeDistributed(Dense(
+    units=512,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+))(x)
+att_v_h3= TimeDistributed(Dense(
+    units=512,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+))(x)
+att_k_h3= TimeDistributed(Dense(
+    units=512,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+))(x)
+
+att_q_h4= TimeDistributed(Dense(
+    units=512,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+))(x)
+att_v_h4= TimeDistributed(Dense(
+    units=512,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+))(x)
+att_k_h4= TimeDistributed(Dense(
+    units=512,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+))(x)
+
+att_q_h5= TimeDistributed(Dense(
+    units=512,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+))(x)
+att_v_h5= TimeDistributed(Dense(
+    units=512,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+))(x)
+att_k_h5= TimeDistributed(Dense(
+    units=512,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+))(x)
+
+att_q_h6= TimeDistributed(Dense(
+    units=512,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+))(x)
+att_v_h6= TimeDistributed(Dense(
+    units=512,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+))(x)
+att_k_h6= TimeDistributed(Dense(
+    units=512,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+))(x)
+
+att_q_h7= TimeDistributed(Dense(
+    units=512,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+))(x)
+att_v_h7= TimeDistributed(Dense(
+    units=512,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+))(x)
+att_k_h7= TimeDistributed(Dense(
+    units=512,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+))(x)
+
+att_q_h8= TimeDistributed(Dense(
+    units=512,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+))(x)
+att_v_h8= TimeDistributed(Dense(
+    units=512,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+))(x)
+att_k_h8= TimeDistributed(Dense(
+    units=512,
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+))(x)
+
+
+att_h1= Attention(
+    use_scale=True,
+    dropout=0.1,
+    dtype=float64,
+)([att_q_h1, att_v_h1, att_k_h1])
+att_h2= Attention(
+    use_scale=True,
+    dropout=0.1,
+    dtype=float64,
+)([att_q_h2, att_v_h2, att_k_h2])
+att_h3= Attention(
+    use_scale=True,
+    dropout=0.1,
+    dtype=float64,
+)([att_q_h3, att_v_h3, att_k_h3])
+att_h4= Attention(
+    use_scale=True,
+    dropout=0.1,
+    dtype=float64,
+)([att_q_h4, att_v_h4, att_k_h4])
+att_h5= Attention(
+    use_scale=True,
+    dropout=0.1,
+    dtype=float64,
+)([att_q_h5, att_v_h5, att_k_h5])
+att_h6= Attention(
+    use_scale=True,
+    dropout=0.1,
+    dtype=float64,
+)([att_q_h6, att_v_h6, att_k_h6])
+att_h7= Attention(
+    use_scale=True,
+    dropout=0.1,
+    dtype=float64,
+)([att_q_h7, att_v_h7, att_k_h7])
+att_h8= Attention(
+    use_scale=True,
+    dropout=0.1,
+    dtype=float64,
+)([att_q_h8, att_v_h8, att_k_h8])
+
+x= Add()([att_h1, att_h2, att_h3, att_h4, att_h5, att_h6, att_h7, att_h8])
+x= Normalization(axis=-1)(x)
+
+
+
+
+ann= Flatten()(x)
+
+
+data_out = Dense(TRAIN_GLOSS, activation=softmax, name='batch_class')(ann)
