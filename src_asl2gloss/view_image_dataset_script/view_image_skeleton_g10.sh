d40966a697cb1ec51da770c39afd2edcc6c74223
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2025-09-27 16:15:38 +0800

    added script to view 1st 10 gloss

---
+++ src_asl2gloss/view_image_dataset_script/view_image_skeleton_g10.sh
@@ -0,0 +1,358 @@
+#!/bin/bash
+
+if command -v imv &>/dev/null ; then
+    # --------------------
+    # train
+    # gloss_id=0
+    echo "________ Train ______________"
+    echo "gloss_id --> 0"
+    imv dataset/wlasl/skeleton_image/69241 2>/dev/null
+    imv dataset/wlasl/skeleton_image/65225 2>/dev/null
+    imv dataset/wlasl/skeleton_image/68011 2>/dev/null
+    imv dataset/wlasl/skeleton_image/68208 2>/dev/null
+    imv dataset/wlasl/skeleton_image/68012 2>/dev/null
+    imv dataset/wlasl/skeleton_image/70266 2>/dev/null
+    imv dataset/wlasl/skeleton_image/07085 2>/dev/null
+    imv dataset/wlasl/skeleton_image/07086 2>/dev/null
+    imv dataset/wlasl/skeleton_image/07087 2>/dev/null
+    imv dataset/wlasl/skeleton_image/07069 2>/dev/null
+    imv dataset/wlasl/skeleton_image/07088 2>/dev/null
+    imv dataset/wlasl/skeleton_image/07089 2>/dev/null
+    imv dataset/wlasl/skeleton_image/07090 2>/dev/null
+    imv dataset/wlasl/skeleton_image/07091 2>/dev/null
+    imv dataset/wlasl/skeleton_image/07068 2>/dev/null
+    imv dataset/wlasl/skeleton_image/07097 2>/dev/null
+    imv dataset/wlasl/skeleton_image/07070 2>/dev/null
+    imv dataset/wlasl/skeleton_image/07071 2>/dev/null
+    imv dataset/wlasl/skeleton_image/07073 2>/dev/null
+    imv dataset/wlasl/skeleton_image/67424 2>/dev/null
+    imv dataset/wlasl/skeleton_image/07074 2>/dev/null
+    imv dataset/wlasl/skeleton_image/07075 2>/dev/null
+    imv dataset/wlasl/skeleton_image/07076 2>/dev/null
+    imv dataset/wlasl/skeleton_image/07077 2>/dev/null
+    imv dataset/wlasl/skeleton_image/07078 2>/dev/null
+    imv dataset/wlasl/skeleton_image/07079 2>/dev/null
+    imv dataset/wlasl/skeleton_image/07080 2>/dev/null
+    imv dataset/wlasl/skeleton_image/07081 2>/dev/null
+    imv dataset/wlasl/skeleton_image/07082 2>/dev/null
+    imv dataset/wlasl/skeleton_image/07084 2>/dev/null
+    # gloss_id=1
+    echo "gloss_id --> 1"
+    imv dataset/wlasl/skeleton_image/65539 2>/dev/null
+    imv dataset/wlasl/skeleton_image/68660 2>/dev/null
+    imv dataset/wlasl/skeleton_image/68041 2>/dev/null
+    imv dataset/wlasl/skeleton_image/17725 2>/dev/null
+    imv dataset/wlasl/skeleton_image/17726 2>/dev/null
+    imv dataset/wlasl/skeleton_image/17727 2>/dev/null
+    imv dataset/wlasl/skeleton_image/17728 2>/dev/null
+    imv dataset/wlasl/skeleton_image/17710 2>/dev/null
+    imv dataset/wlasl/skeleton_image/17729 2>/dev/null
+    imv dataset/wlasl/skeleton_image/17731 2>/dev/null
+    imv dataset/wlasl/skeleton_image/17732 2>/dev/null
+    imv dataset/wlasl/skeleton_image/17733 2>/dev/null
+    imv dataset/wlasl/skeleton_image/65540 2>/dev/null
+    imv dataset/wlasl/skeleton_image/17711 2>/dev/null
+    imv dataset/wlasl/skeleton_image/17712 2>/dev/null
+    imv dataset/wlasl/skeleton_image/17714 2>/dev/null
+    imv dataset/wlasl/skeleton_image/17715 2>/dev/null
+    imv dataset/wlasl/skeleton_image/17717 2>/dev/null
+    imv dataset/wlasl/skeleton_image/17709 2>/dev/null
+    imv dataset/wlasl/skeleton_image/67594 2>/dev/null
+    imv dataset/wlasl/skeleton_image/17719 2>/dev/null
+    imv dataset/wlasl/skeleton_image/17720 2>/dev/null
+    imv dataset/wlasl/skeleton_image/17721 2>/dev/null
+    imv dataset/wlasl/skeleton_image/17722 2>/dev/null
+    imv dataset/wlasl/skeleton_image/17723 2>/dev/null
+    # gloss_id=2
+    echo "gloss_id --> 2"
+    imv dataset/wlasl/skeleton_image/68028 2>/dev/null
+    imv dataset/wlasl/skeleton_image/69054 2>/dev/null
+    imv dataset/wlasl/skeleton_image/12328 2>/dev/null
+    imv dataset/wlasl/skeleton_image/12329 2>/dev/null
+    imv dataset/wlasl/skeleton_image/12330 2>/dev/null
+    imv dataset/wlasl/skeleton_image/12331 2>/dev/null
+    imv dataset/wlasl/skeleton_image/12311 2>/dev/null
+    imv dataset/wlasl/skeleton_image/12337 2>/dev/null
+    imv dataset/wlasl/skeleton_image/12338 2>/dev/null
+    imv dataset/wlasl/skeleton_image/12313 2>/dev/null
+    imv dataset/wlasl/skeleton_image/12314 2>/dev/null
+    imv dataset/wlasl/skeleton_image/12316 2>/dev/null
+    imv dataset/wlasl/skeleton_image/12317 2>/dev/null
+    imv dataset/wlasl/skeleton_image/12318 2>/dev/null
+    imv dataset/wlasl/skeleton_image/12319 2>/dev/null
+    imv dataset/wlasl/skeleton_image/12321 2>/dev/null
+    imv dataset/wlasl/skeleton_image/12322 2>/dev/null
+    imv dataset/wlasl/skeleton_image/12323 2>/dev/null
+    imv dataset/wlasl/skeleton_image/12324 2>/dev/null
+    imv dataset/wlasl/skeleton_image/12327 2>/dev/null
+    # gloss_id=3
+    echo "gloss_id --> 3"
+    imv dataset/wlasl/skeleton_image/05724 2>/dev/null
+    imv dataset/wlasl/skeleton_image/70348 2>/dev/null
+    imv dataset/wlasl/skeleton_image/68007 2>/dev/null
+    imv dataset/wlasl/skeleton_image/05744 2>/dev/null
+    imv dataset/wlasl/skeleton_image/05728 2>/dev/null
+    imv dataset/wlasl/skeleton_image/05747 2>/dev/null
+    imv dataset/wlasl/skeleton_image/05748 2>/dev/null
+    imv dataset/wlasl/skeleton_image/05749 2>/dev/null
+    imv dataset/wlasl/skeleton_image/05729 2>/dev/null
+    imv dataset/wlasl/skeleton_image/05730 2>/dev/null
+    imv dataset/wlasl/skeleton_image/65167 2>/dev/null
+    imv dataset/wlasl/skeleton_image/05731 2>/dev/null
+    imv dataset/wlasl/skeleton_image/05732 2>/dev/null
+    imv dataset/wlasl/skeleton_image/05733 2>/dev/null
+    imv dataset/wlasl/skeleton_image/05734 2>/dev/null
+    imv dataset/wlasl/skeleton_image/05740 2>/dev/null
+    imv dataset/wlasl/skeleton_image/05742 2>/dev/null
+    imv dataset/wlasl/skeleton_image/05743 2>/dev/null
+    # gloss_id=4
+    echo "gloss_id --> 4"
+    imv dataset/wlasl/skeleton_image/70230 2>/dev/null
+    imv dataset/wlasl/skeleton_image/68580 2>/dev/null
+    imv dataset/wlasl/skeleton_image/70263 2>/dev/null
+    imv dataset/wlasl/skeleton_image/68019 2>/dev/null
+    imv dataset/wlasl/skeleton_image/09865 2>/dev/null
+    imv dataset/wlasl/skeleton_image/09866 2>/dev/null
+    imv dataset/wlasl/skeleton_image/09848 2>/dev/null
+    imv dataset/wlasl/skeleton_image/09869 2>/dev/null
+    imv dataset/wlasl/skeleton_image/09849 2>/dev/null
+    imv dataset/wlasl/skeleton_image/09850 2>/dev/null
+    imv dataset/wlasl/skeleton_image/65328 2>/dev/null
+    imv dataset/wlasl/skeleton_image/09853 2>/dev/null
+    imv dataset/wlasl/skeleton_image/09856 2>/dev/null
+    imv dataset/wlasl/skeleton_image/09857 2>/dev/null
+    imv dataset/wlasl/skeleton_image/09858 2>/dev/null
+    imv dataset/wlasl/skeleton_image/09859 2>/dev/null
+    imv dataset/wlasl/skeleton_image/09860 2>/dev/null
+    imv dataset/wlasl/skeleton_image/09861 2>/dev/null
+    imv dataset/wlasl/skeleton_image/09863 2>/dev/null
+    # gloss_id=5
+    echo "gloss_id --> 5"
+    imv dataset/wlasl/skeleton_image/68292 2>/dev/null
+    imv dataset/wlasl/skeleton_image/24955 2>/dev/null
+    imv dataset/wlasl/skeleton_image/24956 2>/dev/null
+    imv dataset/wlasl/skeleton_image/24941 2>/dev/null
+    imv dataset/wlasl/skeleton_image/24961 2>/dev/null
+    imv dataset/wlasl/skeleton_image/24962 2>/dev/null
+    imv dataset/wlasl/skeleton_image/24965 2>/dev/null
+    imv dataset/wlasl/skeleton_image/24969 2>/dev/null
+    imv dataset/wlasl/skeleton_image/24970 2>/dev/null
+    imv dataset/wlasl/skeleton_image/24971 2>/dev/null
+    imv dataset/wlasl/skeleton_image/24972 2>/dev/null
+    imv dataset/wlasl/skeleton_image/65824 2>/dev/null
+    imv dataset/wlasl/skeleton_image/24973 2>/dev/null
+    imv dataset/wlasl/skeleton_image/24943 2>/dev/null
+    imv dataset/wlasl/skeleton_image/24947 2>/dev/null
+    imv dataset/wlasl/skeleton_image/24940 2>/dev/null
+    imv dataset/wlasl/skeleton_image/24950 2>/dev/null
+    imv dataset/wlasl/skeleton_image/24951 2>/dev/null
+    imv dataset/wlasl/skeleton_image/24954 2>/dev/null
+    # gloss_id=6
+    echo "gloss_id --> 6"
+    imv dataset/wlasl/skeleton_image/68870 2>/dev/null
+    imv dataset/wlasl/skeleton_image/68024 2>/dev/null
+    imv dataset/wlasl/skeleton_image/11327 2>/dev/null
+    imv dataset/wlasl/skeleton_image/11328 2>/dev/null
+    imv dataset/wlasl/skeleton_image/11310 2>/dev/null
+    imv dataset/wlasl/skeleton_image/11329 2>/dev/null
+    imv dataset/wlasl/skeleton_image/11330 2>/dev/null
+    imv dataset/wlasl/skeleton_image/11312 2>/dev/null
+    imv dataset/wlasl/skeleton_image/11313 2>/dev/null
+    imv dataset/wlasl/skeleton_image/11309 2>/dev/null
+    imv dataset/wlasl/skeleton_image/11316 2>/dev/null
+    imv dataset/wlasl/skeleton_image/11317 2>/dev/null
+    imv dataset/wlasl/skeleton_image/11320 2>/dev/null
+    imv dataset/wlasl/skeleton_image/11321 2>/dev/null
+    imv dataset/wlasl/skeleton_image/11322 2>/dev/null
+    imv dataset/wlasl/skeleton_image/11323 2>/dev/null
+    imv dataset/wlasl/skeleton_image/11324 2>/dev/null
+    imv dataset/wlasl/skeleton_image/11326 2>/dev/null
+    # gloss_id=7
+    echo "gloss_id --> 7"
+    imv dataset/wlasl/skeleton_image/69534 2>/dev/null
+    imv dataset/wlasl/skeleton_image/68890 2>/dev/null
+    imv dataset/wlasl/skeleton_image/68183 2>/dev/null
+    imv dataset/wlasl/skeleton_image/63239 2>/dev/null
+    imv dataset/wlasl/skeleton_image/63240 2>/dev/null
+    imv dataset/wlasl/skeleton_image/63241 2>/dev/null
+    imv dataset/wlasl/skeleton_image/63242 2>/dev/null
+    imv dataset/wlasl/skeleton_image/63227 2>/dev/null
+    imv dataset/wlasl/skeleton_image/63228 2>/dev/null
+    imv dataset/wlasl/skeleton_image/63231 2>/dev/null
+    imv dataset/wlasl/skeleton_image/66778 2>/dev/null
+    imv dataset/wlasl/skeleton_image/63232 2>/dev/null
+    imv dataset/wlasl/skeleton_image/66779 2>/dev/null
+    imv dataset/wlasl/skeleton_image/66780 2>/dev/null
+    imv dataset/wlasl/skeleton_image/63225 2>/dev/null
+    imv dataset/wlasl/skeleton_image/67066 2>/dev/null
+    imv dataset/wlasl/skeleton_image/63236 2>/dev/null
+    imv dataset/wlasl/skeleton_image/63238 2>/dev/null
+    # gloss_id=8
+    echo "gloss_id --> 8"
+    imv dataset/wlasl/skeleton_image/08909 2>/dev/null
+    imv dataset/wlasl/skeleton_image/68018 2>/dev/null
+    imv dataset/wlasl/skeleton_image/70326 2>/dev/null
+    imv dataset/wlasl/skeleton_image/68790 2>/dev/null
+    imv dataset/wlasl/skeleton_image/08928 2>/dev/null
+    imv dataset/wlasl/skeleton_image/08916 2>/dev/null
+    imv dataset/wlasl/skeleton_image/08917 2>/dev/null
+    imv dataset/wlasl/skeleton_image/08919 2>/dev/null
+    imv dataset/wlasl/skeleton_image/08920 2>/dev/null
+    imv dataset/wlasl/skeleton_image/08921 2>/dev/null
+    imv dataset/wlasl/skeleton_image/08923 2>/dev/null
+    imv dataset/wlasl/skeleton_image/65298 2>/dev/null
+    imv dataset/wlasl/skeleton_image/08924 2>/dev/null
+    imv dataset/wlasl/skeleton_image/65299 2>/dev/null
+    imv dataset/wlasl/skeleton_image/67470 2>/dev/null
+    imv dataset/wlasl/skeleton_image/08926 2>/dev/null
+    imv dataset/wlasl/skeleton_image/08927 2>/dev/null
+    # gloss_id=9
+    echo "gloss_id --> 9"
+    imv dataset/wlasl/skeleton_image/70332 2>/dev/null
+    imv dataset/wlasl/skeleton_image/68592 2>/dev/null
+    imv dataset/wlasl/skeleton_image/13647 2>/dev/null
+    imv dataset/wlasl/skeleton_image/13648 2>/dev/null
+    imv dataset/wlasl/skeleton_image/13631 2>/dev/null
+    imv dataset/wlasl/skeleton_image/13632 2>/dev/null
+    imv dataset/wlasl/skeleton_image/13636 2>/dev/null
+    imv dataset/wlasl/skeleton_image/13637 2>/dev/null
+    imv dataset/wlasl/skeleton_image/13638 2>/dev/null
+    imv dataset/wlasl/skeleton_image/13630 2>/dev/null
+    imv dataset/wlasl/skeleton_image/13640 2>/dev/null
+    imv dataset/wlasl/skeleton_image/13642 2>/dev/null
+    imv dataset/wlasl/skeleton_image/13643 2>/dev/null
+    imv dataset/wlasl/skeleton_image/13644 2>/dev/null
+    imv dataset/wlasl/skeleton_image/13645 2>/dev/null
+    imv dataset/wlasl/skeleton_image/13646 2>/dev/null
+    # gloss_id=10
+    # --------------------
+    # val
+    # gloss_id=0
+    echo "________ Val ______________"
+    echo "gloss_id --> 0"
+    imv dataset/wlasl/skeleton_image/70212 2>/dev/null
+    imv dataset/wlasl/skeleton_image/07094 2>/dev/null
+    imv dataset/wlasl/skeleton_image/07096 2>/dev/null
+    imv dataset/wlasl/skeleton_image/07098 2>/dev/null
+    imv dataset/wlasl/skeleton_image/07099 2>/dev/null
+    imv dataset/wlasl/skeleton_image/07083 2>/dev/null
+    # gloss_id=1
+    echo "gloss_id --> 1"
+    imv dataset/wlasl/skeleton_image/69302 2>/dev/null
+    imv dataset/wlasl/skeleton_image/70173 2>/dev/null
+    imv dataset/wlasl/skeleton_image/68538 2>/dev/null
+    imv dataset/wlasl/skeleton_image/68042 2>/dev/null
+    imv dataset/wlasl/skeleton_image/17734 2>/dev/null
+    imv dataset/wlasl/skeleton_image/17724 2>/dev/null
+    # gloss_id=2
+    echo "gloss_id --> 2"
+    imv dataset/wlasl/skeleton_image/12312 2>/dev/null
+    imv dataset/wlasl/skeleton_image/12333 2>/dev/null
+    imv dataset/wlasl/skeleton_image/12335 2>/dev/null
+    imv dataset/wlasl/skeleton_image/12315 2>/dev/null
+    imv dataset/wlasl/skeleton_image/12326 2>/dev/null
+    # gloss_id=3
+    echo "gloss_id --> 3"
+    imv dataset/wlasl/skeleton_image/05746 2>/dev/null
+    imv dataset/wlasl/skeleton_image/05750 2>/dev/null
+    imv dataset/wlasl/skeleton_image/05736 2>/dev/null
+    imv dataset/wlasl/skeleton_image/05737 2>/dev/null
+    # gloss_id=4
+    echo "gloss_id --> 4"
+    imv dataset/wlasl/skeleton_image/09851 2>/dev/null
+    imv dataset/wlasl/skeleton_image/09854 2>/dev/null
+    imv dataset/wlasl/skeleton_image/67483 2>/dev/null
+    imv dataset/wlasl/skeleton_image/09862 2>/dev/null
+    # gloss_id=5
+    echo "gloss_id --> 5"
+    imv dataset/wlasl/skeleton_image/69345 2>/dev/null
+    imv dataset/wlasl/skeleton_image/24946 2>/dev/null
+    imv dataset/wlasl/skeleton_image/24948 2>/dev/null
+    imv dataset/wlasl/skeleton_image/24952 2>/dev/null
+    # gloss_id=6
+    echo "gloss_id --> 6"
+    imv dataset/wlasl/skeleton_image/11314 2>/dev/null
+    imv dataset/wlasl/skeleton_image/11315 2>/dev/null
+    imv dataset/wlasl/skeleton_image/11319 2>/dev/null
+    imv dataset/wlasl/skeleton_image/11325 2>/dev/null
+    # gloss_id=7
+    echo "gloss_id --> 7"
+    imv dataset/wlasl/skeleton_image/63229 2>/dev/null
+    imv dataset/wlasl/skeleton_image/63230 2>/dev/null
+    imv dataset/wlasl/skeleton_image/63234 2>/dev/null
+    imv dataset/wlasl/skeleton_image/63237 2>/dev/null
+    # gloss_id=8
+    echo "gloss_id --> 8"
+    imv dataset/wlasl/skeleton_image/08929 2>/dev/null
+    imv dataset/wlasl/skeleton_image/08918 2>/dev/null
+    imv dataset/wlasl/skeleton_image/08922 2>/dev/null
+    imv dataset/wlasl/skeleton_image/67468 2>/dev/null
+    # gloss_id=9
+    echo "gloss_id --> 9"
+    imv dataset/wlasl/skeleton_image/13633 2>/dev/null
+    imv dataset/wlasl/skeleton_image/13634 2>/dev/null
+    imv dataset/wlasl/skeleton_image/13639 2>/dev/null
+    imv dataset/wlasl/skeleton_image/13641 2>/dev/null
+    # gloss_id=10
+    # --------------------
+    # test
+    # gloss_id=0
+    echo "________ Test ______________"
+    echo "gloss_id --> 0"
+    imv dataset/wlasl/skeleton_image/07092 2>/dev/null
+    imv dataset/wlasl/skeleton_image/07093 2>/dev/null
+    imv dataset/wlasl/skeleton_image/07095 2>/dev/null
+    imv dataset/wlasl/skeleton_image/07072 2>/dev/null
+    # gloss_id=1
+    echo "gloss_id --> 1"
+    imv dataset/wlasl/skeleton_image/17730 2>/dev/null
+    imv dataset/wlasl/skeleton_image/17713 2>/dev/null
+    imv dataset/wlasl/skeleton_image/17716 2>/dev/null
+    imv dataset/wlasl/skeleton_image/17718 2>/dev/null
+    # gloss_id=2
+    echo "gloss_id --> 2"
+    imv dataset/wlasl/skeleton_image/12306 2>/dev/null
+    imv dataset/wlasl/skeleton_image/12332 2>/dev/null
+    imv dataset/wlasl/skeleton_image/12336 2>/dev/null
+    imv dataset/wlasl/skeleton_image/12320 2>/dev/null
+    imv dataset/wlasl/skeleton_image/67519 2>/dev/null
+    # gloss_id=3
+    echo "gloss_id --> 3"
+    imv dataset/wlasl/skeleton_image/05735 2>/dev/null
+    imv dataset/wlasl/skeleton_image/05727 2>/dev/null
+    imv dataset/wlasl/skeleton_image/05739 2>/dev/null
+    imv dataset/wlasl/skeleton_image/05741 2>/dev/null
+    # gloss_id=4
+    echo "gloss_id --> 4"
+    imv dataset/wlasl/skeleton_image/09847 2>/dev/null
+    imv dataset/wlasl/skeleton_image/09867 2>/dev/null
+    imv dataset/wlasl/skeleton_image/09855 2>/dev/null
+    # gloss_id=5
+    echo "gloss_id --> 5"
+    imv dataset/wlasl/skeleton_image/24857 2>/dev/null
+    imv dataset/wlasl/skeleton_image/24960 2>/dev/null
+    imv dataset/wlasl/skeleton_image/67715 2>/dev/null
+    # gloss_id=6
+    echo "gloss_id --> 6"
+    imv dataset/wlasl/skeleton_image/11305 2>/dev/null
+    imv dataset/wlasl/skeleton_image/11311 2>/dev/null
+    imv dataset/wlasl/skeleton_image/11318 2>/dev/null
+    # gloss_id=7
+    echo "gloss_id --> 7"
+    imv dataset/wlasl/skeleton_image/63219 2>/dev/null
+    imv dataset/wlasl/skeleton_image/63226 2>/dev/null
+    imv dataset/wlasl/skeleton_image/63233 2>/dev/null
+    # gloss_id=8
+    echo "gloss_id --> 8"
+    imv dataset/wlasl/skeleton_image/65300 2>/dev/null
+    imv dataset/wlasl/skeleton_image/08915 2>/dev/null
+    imv dataset/wlasl/skeleton_image/08925 2>/dev/null
+    # gloss_id=9
+    echo "gloss_id --> 9"
+    imv dataset/wlasl/skeleton_image/65415 2>/dev/null
+    imv dataset/wlasl/skeleton_image/13635 2>/dev/null
+    imv dataset/wlasl/skeleton_image/67535 2>/dev/null
+    # gloss_id=10
+else
+    echo "sorry, imv is needed to run this, and your system seem to not have imv installed"
+fi
