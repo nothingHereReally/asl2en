08c2aac5031441d64cdee385aefb3ae9ef775ee4
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2025-10-29 00:01:23 +0800

    changed to use glasl and not wlasl anymore

--- src_asl2gloss/lmark_constant.py
+++ src_asl2gloss/lmark_constant.py
@@ -18,93 +18,41 @@ IMG_SIZE: int= 158 # on G10 mandatory be 158x158x3
 # QUANTITY_FRAME has at least 1 hand( ie. either left
 # or right hand )
 # ie. current is at least 12 or 20 has hand/s 20*0.6= 12
-MIN_FRAMES_HAS_HANDS: int= int(QUANTITY_FRAME*0.7)
-WLASL_SKELETON_DIR: str= f"{PROJ_ROOT}dataset/wlasl/skeleton_image/"
-WLASL_LANDMARK_DIR: str= f"{PROJ_ROOT}dataset/wlasl/landmark_numpy/"
+# MIN_FRAMES_HAS_HANDS: int= int(QUANTITY_FRAME*0.7)
+GLASL_LANDMARK_DIR: str= f"{PROJ_ROOT}dataset/glasl/landmark/"
+GLASL_SKELETON_DIR: str= f"{PROJ_ROOT}dataset/glasl/skeleton/"
 
 
 
 
-tmp_ready: dict= {}
-with open(f"{PROJ_ROOT}dataset/wlasl/wlasl.annotation.skeleton_image.train_val_test.json", 'r') as f:
-    tmp_ready= loadjson(f)
-wlasl_skeleton: dict= tmp_ready.copy()
-del tmp_ready
-SKELETON_IMG_SHAPE: tuple= (158, 158, 3)
+glasl_landmark: dict= {}
+with open(f"{PROJ_ROOT}dataset/glasl/glasl.annotation.landmark.json", "r") as f:
+    glasl_landmark= loadjson(f)
 
-tmp_ready: dict= {}
-with open(f"{PROJ_ROOT}dataset/wlasl/wlasl.annotation.landmark_numpy.train_val_test.json", 'r') as f:
-    tmp_ready= loadjson(f)
-wlasl_landmark: dict= tmp_ready.copy()
-del tmp_ready
-LANDMARK_SHAPE: tuple= (36 +8 +21*2, 2)
+glasl_skeleton: dict= {}
+with open(f"{PROJ_ROOT}dataset/glasl/glasl.annotation.skeleton.json", "r") as f:
+    glasl_skeleton= loadjson(f)
 # landmark is face, then pose, then left_had, then right hand
 # face full is (468, 2)
 # face worthy is (36, 2)
-# pose is (8, 2)
+# pose full is (33, 2)
+# pose worthy is (8, 2)
 # left_hand is (21, 2)
 # right_hand is (21, 2)
 
-K_TRAIN: str= 'train'
-K_VAL: str= 'val'
-K_TEST: str= 'test'
-K_ID2G: str= 'label_id2gloss'
-K_G2ID: str= 'label_gloss2id'
+KEY_TRAIN: str= 'train'
+KEY_VAL: str= 'val'
+KEY_TEST: str= 'test'
+KEY_ID2G: str= 'id2gloss'
+KEY_G2ID: str= 'gloss2id'
 
-T_TRAIN: int= int(len(wlasl_skeleton[K_TRAIN]))
-T_VAL: int= int(len(wlasl_skeleton[K_VAL]))
-T_TEST: int= int(len(wlasl_skeleton[K_TEST]))
-T_GLOSS: int= int(len(wlasl_skeleton[K_ID2G]))
-TRAIN_GLOSS: int= 10
-if T_GLOSS<TRAIN_GLOSS:
-    # TRAIN_GLOSS to be use as to how many gloss be
-    # on training, starting from very 1st gloss till
-    # ${TRAIN_GLOSS}th gloss classifiction on
-    # wlasl_skeleton[K_ID2G] or wlasl_skeleton[K_ID2G]
-    # due to both represent same dataset really
-    raise ValueError({
-        'TRAIN_GLOSS': 'should be less than or equal to T_GLOSS',
-        'value': {
-            'T_GLOSS': T_GLOSS,
-            'TRAIN_GLOSS': TRAIN_GLOSS
-        }
-    })
-# dataset that has only data according to TRAIN_GLOSS
-wlasl_skeleton_TG: dict= {}
-wlasl_landmark_TG: dict= {}
-T_TRAIN_TG: int= 0
-T_VAL_TG: int= 0
-T_TEST_TG: int= 0
-if T_GLOSS!=TRAIN_GLOSS:
-    wlasl_skeleton_TG: dict= {
-        K_TRAIN: [],
-        K_VAL: [],
-        K_TEST: [],
-        K_ID2G: wlasl_skeleton[K_ID2G],
-        K_G2ID: wlasl_skeleton[K_G2ID]
-    }
-    wlasl_landmark_TG: dict= {
-        K_TRAIN: [],
-        K_VAL: [],
-        K_TEST: [],
-        K_ID2G: wlasl_landmark[K_ID2G],
-        K_G2ID: wlasl_landmark[K_G2ID]
-    }
-    for tvt_idv in (K_TRAIN, K_VAL, K_TEST):
-        tillWhat: int= 0
-        while wlasl_landmark[tvt_idv][tillWhat]['gloss_id']<TRAIN_GLOSS:
-            tillWhat+= 1
-        wlasl_skeleton_TG[tvt_idv]= wlasl_skeleton[tvt_idv][0:tillWhat]
-        wlasl_landmark_TG[tvt_idv]= wlasl_landmark[tvt_idv][0:tillWhat]
-    T_TRAIN_TG= len(wlasl_skeleton_TG[K_TRAIN])
-    T_VAL_TG= len(wlasl_skeleton_TG[K_VAL])
-    T_TEST_TG= len(wlasl_skeleton_TG[K_TEST])
+LEN_TRAIN: int= int(len(glasl_skeleton[KEY_TRAIN]))
+LEN_VAL: int= int(len(glasl_skeleton[KEY_VAL]))
+LEN_TEST: int= int(len(glasl_skeleton[KEY_TEST]))
+LEN_GLOSS: int= int(len(glasl_skeleton[KEY_ID2G]))
 
-G10_T_TRAIN: int= 11598 # for getGreaterThan_np_initHasHand
-G10_T_VAL: int= 2727 # for getGreaterThan_np_initHasHand
-
-TRAIN_STEPS: int= int(ceil((T_TRAIN*70)/TRAIN_BATCH)) if T_TRAIN_TG==0 else int(ceil(G10_T_TRAIN/TRAIN_BATCH))
-VAL_STEPS: int= int(ceil((T_VAL*70)/TRAIN_BATCH)) if T_VAL_TG==0 else int(ceil(G10_T_VAL/TRAIN_BATCH))
+TRAIN_STEPS: int= int(ceil(LEN_TRAIN/TRAIN_BATCH))
+VAL_STEPS: int= int(ceil(LEN_VAL/TRAIN_BATCH))
 
 
 FACE_CONNECTIONS_FULL: tuple= (
@@ -193,6 +141,13 @@ WORTHY_FACE_IDX: tuple= (
 # before use of POSE_CONNECTIONS modify landmark 1st
 # modify to use index to be used only: 11,12,13,14,15,16,23,24
 # so new index: 0,1,2,3,4,5,6,7
+POSE_CONNECTIONS_FULL: tuple= ((0, 1), (1, 2), (2, 3), (3, 7), (0, 4), (4, 5),
+                              (5, 6), (6, 8), (9, 10), (11, 12), (11, 13),
+                              (13, 15), (15, 17), (15, 19), (15, 21), (17, 19),
+                              (12, 14), (14, 16), (16, 18), (16, 20), (16, 22),
+                              (18, 20), (11, 23), (12, 24), (23, 24), (23, 25),
+                              (24, 26), (25, 27), (26, 28), (27, 29), (28, 30),
+                              (29, 31), (30, 32), (27, 31), (28, 32))
 POSE_CONNECTIONS: tuple= ((0, 1), (0, 2), (1, 3), (2, 4), (3, 5), (6, 7), (0, 6), (1, 7))
 WORTHY_POSE_IDX: tuple= (11,12,13,14,15,16,23,24)
 
