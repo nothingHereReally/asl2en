91d526de4a0710b5743ae4d7392394722b9dcd0b
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2026-05-14 06:32:23 +0800

    getting ready for trainig model v29

--- src_asl2gloss/model_layers.py
+++ src_asl2gloss/model_layers.py
@@ -83,6 +83,69 @@ att_k_h4= TimeDistributed(Dense(
     dtype=float64,
 ))(x)
 
+att_q_h5= TimeDistributed(Dense(
+    units=LANDMARK_SHAPE[0],
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+))(x)
+att_v_h5= TimeDistributed(Dense(
+    units=LANDMARK_SHAPE[0],
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+))(x)
+att_k_h5= TimeDistributed(Dense(
+    units=LANDMARK_SHAPE[0],
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+))(x)
+
+att_q_h6= TimeDistributed(Dense(
+    units=LANDMARK_SHAPE[0],
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+))(x)
+att_v_h6= TimeDistributed(Dense(
+    units=LANDMARK_SHAPE[0],
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+))(x)
+att_k_h6= TimeDistributed(Dense(
+    units=LANDMARK_SHAPE[0],
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+))(x)
+
+att_q_h7= TimeDistributed(Dense(
+    units=LANDMARK_SHAPE[0],
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+))(x)
+att_v_h7= TimeDistributed(Dense(
+    units=LANDMARK_SHAPE[0],
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+))(x)
+att_k_h7= TimeDistributed(Dense(
+    units=LANDMARK_SHAPE[0],
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+))(x)
+
+att_q_h8= TimeDistributed(Dense(
+    units=LANDMARK_SHAPE[0],
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+))(x)
+att_v_h8= TimeDistributed(Dense(
+    units=LANDMARK_SHAPE[0],
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+))(x)
+att_k_h8= TimeDistributed(Dense(
+    units=LANDMARK_SHAPE[0],
+    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+    dtype=float64,
+))(x)
 
 att_h1= Attention(
     use_scale=True,
@@ -108,29 +171,52 @@ att_h4= Attention(
     dtype=float64,
     name="att_4",
 )([att_q_h4, att_v_h4, att_k_h4])
+att_h5= Attention(
+    use_scale=True,
+    dropout=0.1,
+    dtype=float64,
+    name="att_5",
+)([att_q_h5, att_v_h5, att_k_h5])
+att_h6= Attention(
+    use_scale=True,
+    dropout=0.1,
+    dtype=float64,
+    name="att_6",
+)([att_q_h6, att_v_h6, att_k_h6])
+att_h7= Attention(
+    use_scale=True,
+    dropout=0.1,
+    dtype=float64,
+    name="att_7",
+)([att_q_h7, att_v_h7, att_k_h7])
+att_h8= Attention(
+    use_scale=True,
+    dropout=0.1,
+    dtype=float64,
+    name="att_8",
+)([att_q_h8, att_v_h8, att_k_h8])
 
-
-x= Add()([att_h1, att_h2, att_h3, att_h4])
+x= Add()([att_h1, att_h2, att_h3, att_h4, att_h5, att_h6, att_h7, att_h8])
 
 
 
 
 ann= Flatten()(x)
-ann= Dense(
-    units=int((QUANTITY_FRAME*LANDMARK_SHAPE[0])//2),
-    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
-    dtype=float64,
-)(ann)
-ann= Dense(
-    units=int((QUANTITY_FRAME*LANDMARK_SHAPE[0])//5),
-    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
-    dtype=float64,
-)(ann)
-ann= Dense(
-    units=int((QUANTITY_FRAME*LANDMARK_SHAPE[0])//10),
-    activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
-    dtype=float64,
-)(ann)
+# ann= Dense(
+#     units=int((QUANTITY_FRAME*LANDMARK_SHAPE[0])//2),
+#     activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+#     dtype=float64,
+# )(ann)
+# ann= Dense(
+#     units=int((QUANTITY_FRAME*LANDMARK_SHAPE[0])//5),
+#     activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+#     dtype=float64,
+# )(ann)
+# ann= Dense(
+#     units=int((QUANTITY_FRAME*LANDMARK_SHAPE[0])//10),
+#     activation=ReLU(negative_slope=0.0, max_value=256.0, threshold=0.0),
+#     dtype=float64,
+# )(ann)
 
 
 data_out = Dense(LEN_GLOSS, activation=softmax, name='batch_class')(ann)
