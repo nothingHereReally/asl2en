c5afcee8b8affaa9a6c46c5761c254c040fd7142
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2026-06-23 16:42:50 +0800

    changed for model v35

--- src_asl2gloss/model_train.py
+++ src_asl2gloss/model_train.py
@@ -8,13 +8,14 @@
 # 6) model v12 (4% testing)
 # 7) model v10 (4% testing)
 # from os.path import exists
+# from typing import Any
 from keras.src.losses import sparse_categorical_crossentropy
 from keras.src.models import Model
 from keras.src.optimizers import Adam
 # from keras.src.saving import load_model
 
 from .lmark_constant import EPOCHS, KEY_TRAIN, KEY_VAL, PROJ_ROOT
-from .lmark_essentials import get_data_landmark, STEPS_TRAIN, STEPS_VAL
+from .lmark_essentials import calculate_steps_needed, get_data_landmark
 from .model_layers import data_in, data_out
 from .model_callbacks import d_lr, sTraining, tf_board
 
@@ -37,11 +38,18 @@ if __name__=="__main__":
         epochs=EPOCHS,
         callbacks=[d_lr, sTraining, tf_board],
         validation_data=get_data_landmark(train_val=KEY_VAL),
-        steps_per_epoch=STEPS_TRAIN,
-        validation_steps=STEPS_VAL,
+        steps_per_epoch=calculate_steps_needed(KEY_TRAIN),
+        validation_steps=calculate_steps_needed(KEY_VAL),
         validation_freq=1
     )
     print(f"proj_root {PROJ_ROOT}")
     print(f"model quantity of outputs {model.output_shape[-1]}")
-    model.save(f"{PROJ_ROOT /"model" /"aslvid2gloss_v32.keras"}")
-    # loadModel= load_model(f"{PROJ_ROOT}model/aslvid2gloss_v32.keras")
+    model.save(f"{PROJ_ROOT /"model" /"aslvid2gloss_v35.keras"}")
+    # loadModel: Any= load_model(f"{PROJ_ROOT /"model" /"aslvid2gloss_v34.keras"}")
+    # out: dict= loadModel.evaluate(
+    #     x=get_data_landmark(train_val=KEY_TEST),
+    #     steps=STEPS_TEST,
+    #     return_dict=True
+    # )
+    # for k in out.keys():
+    #     print(f"{k} --> {out[k]}")
