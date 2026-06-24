57cd4bc21beb82eef18d313083ccadc70cfbf138
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2026-06-24 10:19:18 +0800

    updated to use pathlib.Path for p1_get_video_guides.py

--- src_asl2gloss/create_own_dataset/p1_get_video_guides.py
+++ src_asl2gloss/create_own_dataset/p1_get_video_guides.py
@@ -1,20 +1,21 @@
 from json import load as loadjson
 from os.path import exists
+from pathlib import Path
 
 
-PROJ_ROOT: str= f"{"/".join(__file__.rsplit("/")[:-3])}/"
+PROJ_ROOT: Path= Path(__file__).parent.parent.parent
 wlasl_ds: dict= {}
-with open(f"{PROJ_ROOT}dataset/wlasl/wlasl.annotation.clean.json", 'r') as f:
+with open(f"{PROJ_ROOT /"dataset" /"wlasl" /"wlasl.annotation.clean.json"}", 'r') as f:
     wlasl_ds= loadjson(f)
 text_out2file: str= ""
 
 for i in range(10):
     text_out2file= f"{text_out2file}# {wlasl_ds[i]['gloss']}, quantity_videos( {len(wlasl_ds[i]['instances'])} ) --> {i}\n"
     for each_video in wlasl_ds[i]['instances']:
-        if not exists(f"{PROJ_ROOT}dataset/wlasl/videos/{each_video['video_id']}.mp4"):
-            raise FileExistsError(f"file does not exist dataset/wlasl/videos/{each_video['video_id']}.mp4")
-        text_out2file= f"{text_out2file}mpv ./dataset/wlasl/videos/{each_video['video_id']}.mp4\n"
+        if not exists(f"{PROJ_ROOT /"dataset" /"wlasl" /"videos" /each_video['video_id']}.mp4"):
+            raise FileExistsError(f"file does not exist {Path("dataset") /"wlasl" /"videos" /each_video['video_id']}.mp4")
+        text_out2file= f"{text_out2file}mpv {Path("dataset") /"wlasl" /"videos" /each_video['video_id']}.mp4\n"
     text_out2file= f"{text_out2file}\n\n"
 
-with open(f"{PROJ_ROOT}src_asl2gloss/create_own_dataset/p2_play_video_commands.txt", "w") as f:
+with open(f"{PROJ_ROOT /"src_asl2gloss" /"create_own_dataset" /"p2_play_video_commands.txt"}", "w") as f:
     f.write(text_out2file)
