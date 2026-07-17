a0d9c2f807efa4335e0687f7767a659b3e6bdfa8
nothingHereReally <137191381+nothingHereReally@users.noreply.github.com>
2026-07-17 10:01:17 +0800

    updated check_modules.sh for echo to be more clear

--- check_modules.sh
+++ check_modules.sh
@@ -5,37 +5,29 @@
 if [ "$(pwd)/.venv/bin/pip" != "$(which pip)" ]; then
     source .venv/bin/activate
 fi
-
-
 installed="$(pip freeze)"
+check() {
+    local module="$1"
+    local version="$2"
+
+    actual=$(echo "$installed" | grep -iE "^${module}=")
+
+    if [[ "$actual" == "${module}==${version}" ]]; then
+        echo "--OK----> ${module}==${version}"
+    else
+       echo "__NOT_EQUAL__ ${actual} != ${module}==${version} <-- expected"
+    fi
+}
 
 
-echo "$installed" | grep -iE "^mediapipe="
-echo "mediapipe==0.10.21"
-
-echo "$installed" | grep -iE "^opencv-python="
-echo "opencv-python==4.12.0.88"
-
-echo "$installed" | grep -iE "^tensorflow="
-echo "tensorflow==2.19.0"
-
-echo "$installed" | grep -iE "^torch="
-echo "torch==2.7.1"
-
-echo "$installed" | grep -iE "^torchaudio="
-echo "torchaudio==2.7.1"
-
-echo "$installed" | grep -iE "^torchvision="
-echo "torchvision==0.22.1"
-
-echo "$installed" | grep -iE "^happytransformer="
-echo "happytransformer==3.0.0"
-
-echo "$installed" | grep -iE "^tf_keras="
-echo "tf_keras==2.19.0"
-
-echo "$installed" | grep -iE "^keras="
-echo "keras==3.11.2"
-
-echo "$installed" | grep -iE "^pygame="
-echo "pygame==2.6.1"
+# 10 modules
+check mediapipe        "0.10.21"
+check opencv-python    "4.12.0.88"
+check tensorflow       "2.19.0"
+check torch            "2.7.1"
+check torchaudio       "2.7.1"
+check torchvision      "0.22.1"
+check happytransformer "3.0.0"
+check tf_keras         "2.19.0"
+check keras            "3.11.2"
+check pygame           "2.6.1"
